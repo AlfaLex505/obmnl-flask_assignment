b831ff8a3bfe79eeaf5b455f72b31a2a74633f54
Alfa <alexisalfaro505@gmail.com>
2025-09-01 17:42:33 -0400

    Code the update operation of the app

--- app.py
+++ app.py
@@ -17,8 +17,40 @@ def get_transactions():
     return render_template('transactions.html', transactions = transactions)
 
 # Create operation
+@app.route('/add', methods = ['GET', 'POST'])
+def add_transaction():
+    if request.method == 'POST':
+        transactions = {
+            'id': len(transactions) + 1,
+            'date': request.form["date"],
+            'amount': float(request.form["amount"])
+        }
+        transactions.append(transactions)
+        return redirect(url_for('get_transactionss'))
+    return render_template('form.html')
+
 
 # Update operation
+@app.route("/edit/<int:transaction_id>", methods=["GET", "POST"])
+def edit_transaction(transaction_id):
+    if request.method == "POST":
+        date = request.form['date']
+        amount = float(request.form['amount'])
+
+        for transaction in transactions:
+            if transaction['id'] == transaction_id:
+                transaction['date'] = date
+                transaction['amount'] = amount
+                break
+        
+        return redirect(url_for('get_transactions'))
+
+    for transaction in transactions:
+        if transaction['id'] == transaction_id:
+            return render_template('edit.html', transaction = transaction)
+    
+    return {'message': 'Transaction not found'}, 404
+
 
 # Delete operation
 
